be234771173c45be0123361f61233c164aeab3a5
hcvst <hc@vst.io>
2023-10-20 19:55:59 +0200

    change Python version of .venv

--- template-notebook/shell.nix
+++ template-notebook/shell.nix
@@ -9,7 +9,7 @@ pkgs.mkShell {
   ];
   shellHook = ''
     activation_script=./.venv/bin/activate
-    test -f $activation_script || python3.9 -m venv .venv
+    test -f $activation_script || python3.11 -m venv .venv
     source $activation_script
     echo $(tput setab 4)iPython venv environment loaded!$(tput sgr0) 
   '';
